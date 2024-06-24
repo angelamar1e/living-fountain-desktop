fb40da842fb27126e705f11067a9aa73c96e88f7
angelamar1e <angela.delapena06@gmail.com>
2024-06-24 22:57:06 +0800

    retrieved App.xaml

---
+++ Living Fountain/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Living_Fountain
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
