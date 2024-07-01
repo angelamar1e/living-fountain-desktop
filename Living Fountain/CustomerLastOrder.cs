ff354cfa036a8add2eb64fd5f56378f8b357f67c
angelamar1e <angela.delapena06@gmail.com>
2024-07-01 20:36:54 +0800

    complete customer page functions

---
+++ Living Fountain/CustomerLastOrder.cs
@@ -0,0 +1,16 @@
+﻿using Living_Fountain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Living_Fountain
+{
+    public class CustomerLastOrder
+    {
+        public customer Customer { get; set; }
+        public DateOnly? LastOrderDate { get; set; }
+    }
+
+}
--- Living Fountain/Customers.xaml.cs
+++ Living Fountain/Customers.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Living_Fountain.Helpers;
+using Living_Fountain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +18,47 @@ using System.Windows.Shapes;
 
 namespace Living_Fountain
 {
-    /// <summary>
-    /// Interaction logic for Customers.xaml
-    /// </summary>
     public partial class Customers : Page
     {
         public Customers()
         {
             InitializeComponent();
+            GetLastOrders();
+        }
+
+        private void GetLastOrders()
+        {
+            using (var dc = new living_fountainContext())
+            {
+                var customerLastOrders = dc.customers
+                    .Select(customer => new CustomerLastOrder
+                    {
+                        Customer = customer,
+                        LastOrderDate = dc.orders
+                            .Where(o => o.block == customer.block && o.lot == customer.lot && o.phase == customer.phase)
+                            .Max(o => (DateOnly?) o.date)
+                    })
+                    .ToList();
+
+                customerRecords.ItemsSource = customerLastOrders;
+            }
+        }
+
+        private void CustomerRecords_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (customerRecords.SelectedItem is CustomerLastOrder selectedRecord)
+            {
+                var detailPage = new CustomerHistory(selectedRecord.Customer);
+
+                // Access the parent Frame (MainFrame)
+                Frame parentFrame = NavigationHelper.FindParentFrame(this);
+
+                if (parentFrame != null)
+                {
+                    // Navigate MainFrame to Edit_Order.xaml with the id parameter
+                    parentFrame.NavigationService.Navigate(detailPage);
+                }
+            }
         }
     }
 }
