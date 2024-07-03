b5d3c1f6dba56633f4352ece9e5a32c55404ea95
beatrizlyle <beatriz.lyle@gmail.com>
2024-07-03 13:49:25 +0800

    updated edit_order frontend

--- Living Fountain/obj/Debug/net8.0-windows/Living Fountain.AssemblyInfo.cs
+++ Living Fountain/obj/Debug/net8.0-windows/Living Fountain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Living Fountain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a353e421b68d47bd404c0ab8e2e0244d244776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b61a5af78751c7ab0def1996e505d9f9acb55d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Living Fountain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Living Fountain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
