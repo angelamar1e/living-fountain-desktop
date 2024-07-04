2644420a2994e2b625571d7d7b5004f1b0395a90
angelamar1e <angela.delapena06@gmail.com>
2024-07-04 13:29:17 +0800

    new model class for quantity sold per product type

--- Living Fountain/Models/living_fountainContext.cs
+++ Living Fountain/Models/living_fountainContext.cs
@@ -33,7 +33,6 @@ public partial class living_fountainContext : DbContext
     public virtual DbSet<salary_type> salary_types { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=living_fountain;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Living Fountain/Models/product_quantity_sold.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Living_Fountain.Models
+{
+    public class product_quantity_sold
+    {
+        public string ProductType { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
